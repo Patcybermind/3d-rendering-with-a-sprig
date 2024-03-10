9ecccc70396ee991cbb1c7d2a34995e16527c22f
Patcybermind <pato.gonzalezcabrera@gmail.com>
2024-03-10 13:46:57 -0400

    activated blue led and nwo the screen shows up prue sattic

--- firmware/spade/src/rpi/main.c
+++ firmware/spade/src/rpi/main.c
@@ -133,11 +133,11 @@ static void power_lights() {
   pwm_set_gpio_level(pin_num_0, 65535/8);
 
   // right blue light
-  // const pin_num_1 = 4;
-  // gpio_set_function(pin_num_1, GPIO_FUNC_PWM);
-  // uint slice_num_1 = pwm_gpio_to_slice_num(pin_num_1);
-  // pwm_set_enabled(slice_num_1, true);
-  // pwm_set_gpio_level(pin_num_1, 65535/4);
+  const pin_num_1 = 4;
+  gpio_set_function(pin_num_1, GPIO_FUNC_PWM);
+  uint slice_num_1 = pwm_gpio_to_slice_num(pin_num_1);
+  pwm_set_enabled(slice_num_1, true);
+  pwm_set_gpio_level(pin_num_1, 65535/4);
 }
 
 // Entry point for the second core that polls the buttons.
